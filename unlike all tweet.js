9ae75c03c52b3e0d1881f9fed4b00a5fcb9e11bd
cr <65101093+curiositlyxd@users.noreply.github.com>
2021-07-18 04:14:17 -0700

    Update and rename unlike all tweets to unlike all tweet.js

---
+++ unlike all tweet.js
@@ -0,0 +1,8 @@
+// https://twitter.com/[YOUR_USERNAME]/likes
+
+setInterval(() => {
+  for (const d of document.querySelectorAll('div[data-testid="unlike"]')) {
+    d.click()
+  }
+  window.scrollTo(0, document.body.scrollHeight)
+}, 1000)
